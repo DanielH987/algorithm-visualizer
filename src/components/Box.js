9a461e76e25cf0de3f2dadd1ec2496808197d77b
Daniel Hootini <hootinid@gmail.com>
2024-08-06 17:03:59 -1000

    Refactor Box component to enable drag and drop functionality

---
+++ src/components/Box.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useDrag, useDrop } from 'react-dnd';
+
+const Box = ({ value, index, moveBox }) => {
+    const [{ isDragging }, dragRef] = useDrag({
+        type: 'BOX',
+        item: { index },
+        collect: (monitor) => ({
+            isDragging: monitor.isDragging(),
+        }),
+    });
+
+    const [, dropRef] = useDrop({
+        accept: 'BOX',
+        hover: (item) => {
+            if (item.index !== index) {
+                moveBox(item.index, index);
+                item.index = index;
+            }
+        },
+    });
+
+    return (
+        <div
+            ref={(node) => dragRef(dropRef(node))}
+            className={`box ${isDragging ? 'is-dragging' : ''}`}
+        >
+            {value}
+        </div>
+    );
+};
+
+export default Box;
