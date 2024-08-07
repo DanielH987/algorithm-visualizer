868b5394a203f789ad83e7963729633db80aa73d
Daniel Hootini <hootinid@gmail.com>
2024-08-07 13:39:10 -1000

    feat: Add lazy loading for images in LongCommonSub component

--- src/components/LongCommonSub/LongCommonSub.js
+++ src/components/LongCommonSub/LongCommonSub.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import './LongCommonSub.css';
+import Cell from './Cell';
+
 
 const generateRandomString = (length) => {
     let result = '';
@@ -12,16 +15,55 @@ const generateRandomString = (length) => {
 const LongCommonSub = () => {
     const [string1, setString1] = useState('');
     const [string2, setString2] = useState('');
+    const numRows = 13;
+    const numCols = 13;
+    const [tableData, setTableData] = useState([]);
 
     useEffect(() => {
         setString1(generateRandomString(11));
         setString2(generateRandomString(11));
     }, []);
 
+    useEffect(() => {
+        if (string1 && string2) {
+            const rows = [];
+            for (let i = 0; i < numRows; i++) {
+                const cells = [];
+                for (let j = 0; j < numCols; j++) {
+                    if (i === 0 && j > 1) {
+                        cells.push(string1[j - 2]);
+                    } else if (i === 1 && j > 0) {
+                        cells.push(0);
+                    } else if (i > 1 && j === 0) {
+                        cells.push(string2[i - 2]);
+                    } else if (i > 0 && j === 1) {
+                        cells.push(0);
+                    }
+                    else {
+                        cells.push(null);
+                    }
+                }
+                rows.push(cells);
+            }
+            setTableData(rows);
+        }
+    }, [string1, string2, numRows, numCols]);
+
     return (
-        <div>
+        <div className="container">
             <h2>Preparation for Quiz QLCS</h2>
             <h3>{string1} and {string2}</h3>
+            <table>
+                <tbody>
+                    {tableData.map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                            {row.map((cellValue, cellIndex) => (
+                                <Cell key={cellIndex} value={cellValue} />
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
