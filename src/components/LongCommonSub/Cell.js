d4c3ddc5d2b5bdf394384dfa1ebb06e6df2d211c
Daniel Hootini <hootinid@gmail.com>
2024-08-07 14:26:21 -1000

    feat: Improve Cell component in LongCommonSub by adding input fields for values greater than 1

--- src/components/LongCommonSub/Cell.js
+++ src/components/LongCommonSub/Cell.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './Cell.css';
 
-const Cell = ({ value }) => {
+const Cell = ({ value, onChange, rowIndex, cellIndex }) => {
+    const handleChange = (e) => {
+        onChange(rowIndex, cellIndex, e.target.value);
+    };
+
     return (
-        <td className="cell">{value}</td>
+        <td className="cell">
+            {rowIndex > 1 && cellIndex > 1 ? (
+                <input
+                    type="number"
+                    value={value || ''}
+                    onChange={handleChange}
+                    className="number-input"
+                />
+            ) : (
+                value
+            )}
+        </td>
     );
 };
 
