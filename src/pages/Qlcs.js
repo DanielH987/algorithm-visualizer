53673b3cd0c3c32bf3d3adbdf8aa9f3bb338161c
Daniel Hootini <hootinid@gmail.com>
2024-08-08 11:16:51 -1000

    Refactor QLCS page modal message for algorithm completion

--- src/pages/Qlcs.js
+++ src/pages/Qlcs.js
@@ -18,7 +18,7 @@ const Qlcs = () => {
       <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
         <h2>Welcome to the QLCS Page!</h2>
         <p>Given two strings, discover the longest common subsequence (LCS). Report both the length of the LCS and a sample common subsequence of that length.</p>
-        <p>You'll see a "Correct!" message once you've successfully completed the task.</p>
+        <p>You'll see a "Correct!" message once you've successfully completed the algorithm.</p>
         <p>Click "Generate New" to create a new practice quiz.</p>
         <p>Tap anywhere to close this message.</p>
       </Modal>
