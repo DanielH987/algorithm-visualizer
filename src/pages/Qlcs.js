f94103c0ce86dafa5fb14d6b3384b2c32f40db7d
Daniel Hootini <hootinid@gmail.com>
2024-08-09 15:30:18 -1000

    Refactor QLCS page to include help button in modal

--- src/pages/Qlcs.js
+++ src/pages/Qlcs.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import LongCommonSub from '../components/LongCommonSub/LongCommonSub';
+import { FaEyeSlash } from 'react-icons/fa';
 import Modal from '../components/Modal';
 
 const Qlcs = () => {
@@ -18,7 +19,7 @@ const Qlcs = () => {
       <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
         <h2>Welcome to the QLCS Page!</h2>
         <p>Given two strings, discover the longest common subsequence (LCS). Report both the length of the LCS and a sample common subsequence of that length.</p>
-        <p>You'll see a "Correct!" message once you've successfully completed the algorithm.</p>
+        <p>You'll see a "Correct!" message once you've successfully completed the algorithm. Press the <FaEyeSlash /> button for help.</p>
         <p>Click "Generate New" to create a new practice quiz.</p>
         <p>Tap anywhere to close this message.</p>
       </Modal>
