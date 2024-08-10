a7800579bad27b5a2a3f977112be62fed635194c
Daniel Hootini <hootinid@gmail.com>
2024-08-09 15:30:28 -1000

    feat: Add eye slash button for help in QQP modal

--- src/pages/Qqp.js
+++ src/pages/Qqp.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import QuicksortPartition from '../components/Quicksort/QuicksortPartition';
+import { FaEyeSlash } from 'react-icons/fa';
 import Modal from '../components/Modal';
 
 const Qqp = () => {
@@ -19,7 +20,7 @@ const Qqp = () => {
       <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
         <h2>Welcome to the QQP Page!</h2>
         <p>Use the PARTITION algorithm to transform the arrays of numbers provided, using the last number as the pivot. After arranging the numbers, click the "Show Answer" button to check your work.</p>
-        <p>You'll see a "Correct!" message once you've successfully completed the algorithm.</p>
+        <p>You'll see a "Correct!" message once you've successfully completed the algorithm. Press the <FaEyeSlash /> button for help.</p>
         <p>Click "Generate New List" to create a new practice quiz.</p>
         <p>Tap anywhere to close this message.</p>
       </Modal>
