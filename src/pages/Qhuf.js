2dcf4bebec3cf0c15f67fcb10832a03b5ec1e7f2
Daniel Hootini <hootinid@gmail.com>
2024-08-09 16:45:11 -1000

    feat: Add modal to Qhuf page with instructions for practice quiz

--- src/pages/Qhuf.js
+++ src/pages/Qhuf.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Huffman from '../components/Huffman/Huffman';
+import Modal from '../components/Modal';
 
 const Qhuf = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    setIsModalOpen(true);
+  }, []);
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div>
-      <h2>Qhuf</h2>
-      <p>Discover Qhuf.</p>
+      <Huffman />
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+        <h2>Welcome to the QHUF Page!</h2>
+        <p>(1) Report the total bits used (sum of length times frequency for each letter).</p>
+        <p>(2) Show a correct Huffman code for each letter.</p>
+        <p>You'll see a "Correct!" message once you've successfully calculated the total bits and generated the Huffman codes.</p>
+        <p>Click "Generate New" to create a new practice quiz.</p>
+        <p>Tap anywhere to close this message.</p>
+      </Modal>
     </div>
   );
 };
