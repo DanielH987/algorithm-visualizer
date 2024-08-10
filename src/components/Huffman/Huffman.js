235a5cc8483756d229cbe2eb0157d45da91ba995
Daniel Hootini <hootinid@gmail.com>
2024-08-09 16:57:20 -1000

    Refactor Huffman component to include toggle for showing answer

--- src/components/Huffman/Huffman.js
+++ src/components/Huffman/Huffman.js
@@ -12,6 +12,7 @@ const Huffman = () => {
 
     const [encodingInputs, setEncodingInputs] = useState(Array(7).fill(''));
     const [bitLengthInputs, setBitLengthInputs] = useState(Array(7).fill(''));
+    const [showAnswer, setShowAnswer] = useState(false);
 
     function generateRandomCharacters() {
         return Array.from({ length: 7 }, () => String.fromCharCode(Math.floor(Math.random() * 26) + 65));
@@ -42,11 +43,54 @@ const Huffman = () => {
         setRandomNumbers(generateRandomNumbers());
         setEncodingInputs(Array(7).fill(''));
         setBitLengthInputs(Array(7).fill(''));
+        setShowAnswer(false);
+    };
+
+    const toggleShowAnswer = () => {
+        setShowAnswer(!showAnswer);
     };
 
     const calculateTotalBitLength = () => {
         return bitLengthInputs.reduce((total, num) => total + (parseInt(num, 10) || 0), 0);
     };
+
+    function calculateTotalBitLengthUsingHuffman() {
+        const characters = randomCharacters.map((char, index) => ({
+            char,
+            freq: randomNumbers[index]
+        }));
+    
+        while (characters.length > 1) {
+            characters.sort((a, b) => a.freq - b.freq);
+            const left = characters.shift();
+            const right = characters.shift();
+            const newNode = {
+                char: left.char + right.char,
+                freq: left.freq + right.freq,
+                left,
+                right,
+            };
+            characters.push(newNode);
+        }
+    
+        const huffmanTree = characters[0];
+    
+        function assignBitLengths(node, length) {
+            if (!node.left && !node.right) {
+                return { [node.char]: length };
+            }
+            return {
+                ...(node.left ? assignBitLengths(node.left, length + 1) : {}),
+                ...(node.right ? assignBitLengths(node.right, length + 1) : {}),
+            };
+        }
+    
+        const bitLengths = assignBitLengths(huffmanTree, 0);
+    
+        return randomCharacters.reduce((total, char, index) => {
+            return total + (bitLengths[char] * randomNumbers[index]);
+        }, 0);
+    }    
 
     return (
         <div className="huffman-container">
@@ -92,7 +136,19 @@ const Huffman = () => {
                     </tr>
                 </tbody>
             </table>
-            <button className='styled-button' onClick={regenerate}>Generate New</button>
+
+            <div className='styled-container'>
+                <button className='styled-button' onClick={regenerate}>Generate New</button>
+                <button className='styled-button' onClick={toggleShowAnswer}>
+                    {showAnswer ? 'Hide Answer' : 'Show Answer'}
+                </button>
+            </div>
+
+            {showAnswer && (
+                <h3>
+                    Total Bit Length: {calculateTotalBitLengthUsingHuffman()}
+                </h3>
+            )}
         </div>
     );
 }
