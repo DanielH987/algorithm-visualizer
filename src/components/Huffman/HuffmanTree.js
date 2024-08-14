c617ecaac05fc18f9d4c601353ddebeeaa8cd46b
Daniel Hootini <hootinid@gmail.com>
2024-08-14 10:05:05 -1000

    refactor: Add dropdown menu for node options in HuffmanTree component

--- src/components/Huffman/HuffmanTree.js
+++ src/components/Huffman/HuffmanTree.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useDrag, useDrop } from 'react-dnd';
 import './HuffmanTree.css';
 
-const Node = ({ number, index, moveNode }) => {
+const Node = ({ number, index, moveNode, onNodeDrop }) => {
     const [isHovered, setIsHovered] = useState(false);
 
     const [, drag] = useDrag(() => ({
@@ -22,6 +22,7 @@ const Node = ({ number, index, moveNode }) => {
             setIsHovered(false);
             if (draggedItem.index !== index) {
                 moveNode(draggedItem.index, index);
+                onNodeDrop(index); // Trigger the dropdown menu
             }
         },
         collect: (monitor) => {
@@ -43,6 +44,8 @@ const Node = ({ number, index, moveNode }) => {
 
 const HuffmanTree = ({ randomNumbers }) => {
     const [numbers, setNumbers] = useState(randomNumbers);
+    const [dropdownPosition, setDropdownPosition] = useState(null);
+    const [showDropdown, setShowDropdown] = useState(false);
 
     const moveNode = (fromIndex, toIndex) => {
         setNumbers((prevNumbers) => {
@@ -51,7 +54,19 @@ const HuffmanTree = ({ randomNumbers }) => {
             return updatedNumbers;
         });
     };
-    
+
+    const handleNodeDrop = (index) => {
+        const nodeElement = document.querySelectorAll('.node')[index];
+        const rect = nodeElement.getBoundingClientRect();
+        setDropdownPosition({ top: rect.bottom, left: rect.left });
+        setShowDropdown(true);
+    };
+
+    const handleOptionSelect = (option) => {
+        console.log(`Option selected: ${option}`);
+        setShowDropdown(false);
+    };
+
     return (
         <div className="huffman-tree">
             {numbers.map((number, index) => (
@@ -60,8 +75,17 @@ const HuffmanTree = ({ randomNumbers }) => {
                     index={index}
                     number={number}
                     moveNode={moveNode}
+                    onNodeDrop={handleNodeDrop}
                 />
             ))}
+            {showDropdown && (
+                <div className="dropdown-menu" style={{ top: dropdownPosition.top, left: dropdownPosition.left }}>
+                    <ul>
+                        <li onClick={() => handleOptionSelect('Option 1')}>Move Node</li>
+                        <li onClick={() => handleOptionSelect('Option 2')}>Add Nodes</li>
+                    </ul>
+                </div>
+            )}
         </div>
     );
 }
