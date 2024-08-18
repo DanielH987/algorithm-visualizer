2ef72efa9104f4599bb87e442750da6be1e8e6fd
Daniel Hootini <hootinid@gmail.com>
2024-08-18 10:00:00 -1000

    feat: Add "Coming out soon!" message to Qbst page

--- src/pages/Qbst.js
+++ src/pages/Qbst.js
@@ -14,8 +14,10 @@ const Qbst = () => {
 
   return (
     <div>
+      <h3>Coming out soon!</h3>
       <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
         <h2>Welcome to the Qbst Page!</h2>
+        <p>Coming out soon!</p>
       </Modal>
     </div>
   );
