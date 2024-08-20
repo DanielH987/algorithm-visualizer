ce9cc068bdd1fb26974d28b258d44e6ecc66bfa8
DanielH987 <hootinid@go.byuh.edu>
2024-08-20 09:58:08 -1000

    feat: Add Test component for Huffman page

--- src/pages/Qhuf.js
+++ src/pages/Qhuf.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Huffman from '../components/Huffman/Huffman';
 import HuffmanTree from '../components/Huffman/HuffmanTree';
 import Modal from '../components/Modal/Modal';
+import Test from '../components/Huffman/Test';
 
 const Qhuf = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -85,11 +86,12 @@ const Qhuf = () => {
         {!showAnswer && <button className='styled-button' onClick={toggleAnswer}>Show Answer</button>}
       </div>
 
-      <HuffmanTree 
+      {/* <HuffmanTree 
         key={randomNumbers.join('-')}
         randomCharacters={randomCharacters} 
         randomNumbers={randomNumbers} 
-      />
+      /> */}
+      <Test />
 
       <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
         <h2>Welcome to the QHUF Page!</h2>
