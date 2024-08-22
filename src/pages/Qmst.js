5692ed10281451ed91eda12dcc282488e6b64dfb
Daniel Hootini <hootinid@gmail.com>
2024-08-22 13:48:49 -1000

    feat: Update Qmst page to generate random graph data with increased weight range

--- src/pages/Qmst.js
+++ src/pages/Qmst.js
@@ -16,7 +16,7 @@ const generateGraphData = (numNodes) => {
 
   for (let i = 0; i < limitedNumNodes; i++) {
     for (let j = i + 1; j < limitedNumNodes; j++) {
-      const weight = Math.floor(Math.random() * 20) + 1;
+      const weight = Math.floor(Math.random() * 30) + 1;
       links.push({
         source: nodes[i].id,
         target: nodes[j].id,
@@ -38,8 +38,9 @@ const Qmst = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [graphData, setGraphData] = useState({ nodes: [], links: [] });
   const [graphText, setGraphText] = useState('');
-  const [clickedNodes, setClickedNodes] = useState([]);
-  const [clickedLinks, setClickedLinks] = useState([]); // Store clicked links and weights
+  const [clickedVertices, setClickedVertices] = useState([]);
+  const [clickedEdges, setClickedEdges] = useState([]);
+  const [totalWeight, setTotalWeight] = useState(0);
 
   useEffect(() => {
     setIsModalOpen(true);
@@ -55,8 +56,8 @@ const Qmst = () => {
     setIsModalOpen(false);
   };
 
-  const handleNodeClick = (node) => {
-    setClickedNodes((prevNodes) => {
+  const handleVertxClick = (node) => {
+    setClickedVertices((prevNodes) => {
       if (prevNodes.includes(node.id)) {
         return prevNodes.filter(n => n !== node.id);
       } else {
@@ -65,14 +66,24 @@ const Qmst = () => {
     });
   };
 
-  const handleLinkClick = (link) => {
+  const handleEdgeClick = (link) => {
     const linkInfo = `${link.source.id}-${link.target.id} (Weight: ${link.weight})`;
-    setClickedLinks((prevLinks) => {
+    
+    setClickedEdges((prevLinks) => {
+      let updatedLinks;
       if (prevLinks.includes(linkInfo)) {
-        return prevLinks.filter(l => l !== linkInfo);
+        updatedLinks = prevLinks.filter(l => l !== linkInfo);
       } else {
-        return [...prevLinks, linkInfo];
+        updatedLinks = [...prevLinks, linkInfo];
       }
+
+      const updatedTotalWeight = updatedLinks.reduce((sum, l) => {
+        const weight = parseInt(l.match(/\(Weight: (\d+)\)/)[1], 10);
+        return sum + weight;
+      }, 0);
+
+      setTotalWeight(updatedTotalWeight);
+      return updatedLinks;
     });
   };
 
@@ -81,15 +92,33 @@ const Qmst = () => {
       <div className="qmst-text-container">
         <h2>Preparation for Quiz QMST</h2>
         <pre className="qmst-pre">{graphText}</pre>
-        <div>Clicked Nodes: {clickedNodes.join(', ')}</div>
-        <div>Clicked Links: {clickedLinks.join(', ')}</div> {/* Display clicked links */}
+        <div className="qmst-columns">
+          <div className="qmst-left-column">
+            <h4>Clicked Vertices:</h4>
+            <p style={{ textAlign: 'left' }}>{clickedVertices.join(', ')}</p>
+
+            <h4>Clicked Edges:</h4>
+            <ul>
+              {clickedEdges.map((link, index) => (
+                <li key={index}>{link}</li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="qmst-right-column">
+            <div className='total-weight-container'>
+              <h4>Total Weight (Auto-Calculated):</h4>
+              <p>{totalWeight}</p>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="qmst-graph-container">
         <MinSpanTree
           nodes={graphData.nodes}
           links={graphData.links}
-          onNodeClick={handleNodeClick}
-          onLinkClick={handleLinkClick} // Pass the link click handler
+          onNodeClick={handleVertxClick}
+          onLinkClick={handleEdgeClick}
         />
       </div>
       <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
