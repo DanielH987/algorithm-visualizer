2328afd1d7ef4159dd0002d24ff0cf75364aeadd
Daniel Hootini <hootinid@gmail.com>
2024-08-22 17:06:39 -1000

    refactor: Update navigation links in Nav.js for better consistency and readability

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -16,12 +16,12 @@ const Nav = () => {
         <div></div>
       </div>
       <ul className={isOpen ? 'show' : ''}>
-        <li><Link to="/qhp" onClick={toggleNav}>QHP</Link></li>
-        <li><Link to="/qqp" onClick={toggleNav}>QQP</Link></li>
-        <li><Link to="/qbst" onClick={toggleNav}>QBST</Link></li>
-        <li><Link to="/qlcs" onClick={toggleNav}>QLCS</Link></li>
-        <li><Link to="/qhuf" onClick={toggleNav}>QHUF</Link></li>
-        <li><Link to="/qmst" onClick={toggleNav}>QMST</Link></li>
+        <li><Link to="/build-max-heap" onClick={toggleNav}>QHP</Link></li>
+        <li><Link to="/partition" onClick={toggleNav}>QQP</Link></li>
+        <li><Link to="/binary-search-tree" onClick={toggleNav}>QBST</Link></li>
+        <li><Link to="/longest-common-subsequence" onClick={toggleNav}>QLCS</Link></li>
+        <li><Link to="/huffman-encoding" onClick={toggleNav}>QHUF</Link></li>
+        <li><Link to="/minimum-spanning-tree" onClick={toggleNav}>QMST</Link></li>
       </ul>
     </nav>
   );
