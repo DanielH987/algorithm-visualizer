91567e1d345a3984eb43be67c7b23d50f67cb57f
Daniel Hootini <hootinid@gmail.com>
2024-08-24 10:00:00 -1000

    refactor: Update Footer component to dynamically display current year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <footer>
       <p>&copy; 2024 Daniel Hootini. All rights reserved.</p>
