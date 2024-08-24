21753a11bd7ff57e1620f7dea179b687f4f2a7af
Daniel Hootini <hootinid@gmail.com>
2024-08-23 15:57:47 -1000

    refactor: Update QLCS page modal instructions for better clarity and emphasis

--- src/pages/Qlcs.js
+++ src/pages/Qlcs.js
@@ -18,9 +18,9 @@ const Qlcs = () => {
       <LongCommonSub />
       <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
         <h2>Welcome to the QLCS Page!</h2>
-        <p>Given two strings, discover the longest common subsequence (LCS). Report both the length of the LCS and a sample common subsequence of that length.</p>
-        <p>You'll see a "Correct!" message once you've successfully completed the algorithm. Press the <FaEyeSlash /> button for help.</p>
-        <p>Click "Generate New" to create a new practice quiz.</p>
+        <p>Given two strings, find the <strong>longest common subsequence (LCS)</strong>. Report both the length of the LCS and an example subsequence of that length.</p>
+        <p>You'll see a <strong>"Correct!"</strong> message once you've successfully completed the algorithm. Press the <FaEyeSlash /> button for help.</p>
+        <p>Click <strong>"Generate New"</strong> to create a new practice quiz.</p>
         <p>Tap anywhere to close this message.</p>
       </Modal>
     </div>
